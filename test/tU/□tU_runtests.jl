394f85f20a9d269e96b7c42398b006e68334b1b0
KAP424 <kap.4116@gmail.com>
2026-01-20 21:33:48 +0800

    seem good?

--- test/tU/□tU_runtests.jl
+++ test/tU/□tU_runtests.jl
@@ -41,30 +41,30 @@ using LinearAlgebra
 
     # # ------------------------------------------------------------------------------------------------------------------------------------------------------
 
-    # model = tU_Hubbard_Para(Ht=1.0, Hu1=4.0, Hu2=4.0, Θrelax=1.0, Θquench=0.0, Lattice="SQUARE90",
-    #     site=[3, 3], Δt=0.1, BatchSize=10, Initial="H0", flux=pi)
+    model = tU_Hubbard_Para(Ht=1.0, Hu1=4.0, Hu2=4.0, Θrelax=1.0, Θquench=0.0, Lattice="SQUARE90",
+        site=[3, 3], Δt=0.1, BatchSize=10, Initial="H0", flux=pi)
 
-    # s = Initial_s(model, rng)
+    s = Initial_s(model, rng)
 
-    # s = phy_update(path, model, s, 10, true)
-
-
-    # L = model.site[2]
-    # indexA = area_index(model.Lattice, model.site, ([1, 1], [div(L, 2), L]))
-    # # # HalfHalf
-    # indexB = area_index(model.Lattice, model.site, ([1, 1], [div(L, 2), div(L, 2)]))
-    # # println(indexB)
-
-    # λ = 0.5
-    # Nλ = 2
-
-    # s = ctrl_SCDOPicr(path, model, π / 2, indexA, indexB, 20, λ, Nλ, s, true)
+    s = phy_update(path, model, s, 10, true)
 
 
-    # ss = [copy(s), copy(s)]
+    L = model.site[2]
+    indexA = area_index(model.Lattice, model.site, ([1, 1], [div(L, 2), L]))
+    # # HalfHalf
+    indexB = area_index(model.Lattice, model.site, ([1, 1], [div(L, 2), div(L, 2)]))
+    # println(indexB)
+
+    λ = 0.5
+    Nλ = 2
+
+    s = ctrl_SCDOPicr(path, model, π / 2, indexA, indexB, 20, λ, Nλ, s, true)
 
 
-    # # println(@btime ctrl_SCEEicr($path,$model,$indexA,$indexB,$Sweeps,$λ,$Nλ,$ss,$true) )
-    # ss = ctrl_SCEEicr(path, model, indexA, indexB, 2, λ, Nλ, ss, true)
+    ss = [copy(s), copy(s)]
+
+
+    # println(@btime ctrl_SCEEicr($path,$model,$indexA,$indexB,$Sweeps,$λ,$Nλ,$ss,$true) )
+    ss = ctrl_SCEEicr(path, model, indexA, indexB, 2, λ, Nλ, ss, true)
 
 end
